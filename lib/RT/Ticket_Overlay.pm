25ff4583996e8c6712b023210bced7bcdc6358dd
Ruslan Zakirov <Ruslan.Zakirov@gmail.com>
2009-07-29 02:12:53 +0400

    * limit when user has no rights

--- lib/RT/Ticket_Overlay.pm
+++ lib/RT/Ticket_Overlay.pm
@@ -3470,6 +3470,13 @@ sub Transactions {
             );
 
         }
+    } else {
+        $transactions->Limit(
+            SUBCLAUSE => 'acl',
+            FIELD    => 'id',
+            VALUE    => 0,
+            ENTRYAGGREGATOR => 'AND'
+        );
     }
 
     return ($transactions);
