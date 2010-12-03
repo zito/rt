0573b2638a608448aa7b67ed6418b3f67d47fdf6
Thomas Sibley <trs@bestpractical.com>
2010-12-03 13:36:15 -0500

    Make a note of options we should enable in the (hopefully) near future

--- lib/RT/Interface/Email.pm
+++ lib/RT/Interface/Email.pm
@@ -1859,6 +1859,11 @@ sub ConvertHTMLToText {
             after_link      => ' (%l)',
             footnote        => '',
             skip_linked_urls => 1,
+            # XXX TODO: These should be enabled once the patch (rt.cpan.org
+            # #63571) to HTML::FormatText::WithLinks is accepted and released
+            #with_emphasis   => 1,
+            #bold_marker     => '"',
+            #italic_marker   => '"',
         }
     );
 }
